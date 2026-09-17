0133ee4754bd1cba051aaa5aa414cd61300adb6a
Samantha Taylormoore <staylormoore@gmail.com>
2026-09-16 21:35:19 -0400

    vrf.mjs setup

---
+++ vrf.mjs
@@ -0,0 +1,15 @@
+let privateKey;
+let publicKey;
+let seed;
+
+export function initializeVRF() {
+    // TODO
+}
+
+export function generate(i) {
+    // TODO
+}
+
+export function verify(ri, pi) {
+    // TODO
+}
